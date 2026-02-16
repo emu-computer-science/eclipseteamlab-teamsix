9308f2069ce879b313bd4a00d058555e9a182122
jetercero <jtercero@emich.edu>
2026-02-16 16:42:43 -0500

    Added Hungry java file

--- src/eclipseLab_Team6/Driver.java
+++ src/eclipseLab_Team6/Driver.java
@@ -4,6 +4,10 @@ public class Driver {
 
 	public static void main(String[] args) {
 		System.out.println("Our team is getting started.");
+		
+		Hungry food = new Hungry();
+		
+		food.Hungry();
 	}
 
 }
---
+++ src/eclipseLab_Team6/Hungry.java
@@ -0,0 +1,9 @@
+package eclipseLab_Team6;
+
+public class Hungry {
+	
+	public static void Hungry() {
+		System.out.println("We are all hungry!");
+	}
+
+}
